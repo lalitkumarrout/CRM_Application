0105ed9d0248d3268778271dc4dc60830e7795b4
Lalit kumar Rout <routlalit172@gmail.com>
2025-02-28 23:50:14 +0530

    added the reacter router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import Login from "./pages/auth/login";
 import Signup from "./pages/auth/Signup";
+import MainRoutes from "./routing/MainRoutes";
 
 function App() {
   return (
     <h1 className="text-3xl font-bold">
-   
-    <Signup/>
+      <MainRoutes />
     </h1>
-  )
+  );
 }
 
 export default App;
