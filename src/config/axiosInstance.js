224db607b9fe7401462ae083b1692b9dffd0a4be
Lalit kumar Rout <routlalit172@gmail.com>
2025-03-02 02:33:13 +0530

    login api call

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = import.meta.env.VITE_BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+//axiosInstance.defaults.withCredentials = true;
 axiosInstance.defaults.timeout = import.meta.env.VITE_API_TIMEOUT;
 
 export default axiosInstance;
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../Redux/Slices/AuthSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const [loginDetails, setLoginDetails] = useState({
+    email: "",
+    password: "",
+  });
+  function handleInputChange(e) {
+    const {name,value}=e.target;
+    //console.log(name,value);
+    setLoginDetails({
+      ...loginDetails,
+      [name]:value
+    })
+    
+  }
+
+   function onSubmit() {
+    if(!loginDetails.email || !loginDetails.password) return;
+    console.log(loginDetails)
+    try {
+      const response =  dispatch(login(loginDetails));
+      console.log(response);
+    } catch (error) {
+      console.error("Failed to login:", error);
+    }
+  }
   return (
     <div className="flex justify-center items-center h-[90vh]">
       <div className="card bg-primary text-primary-content w-96">
@@ -11,23 +38,32 @@ const Login = () => {
 
           <div className="w-full">
             <input
+              onChange={handleInputChange}
+              name="email"
               autoComplete="one-time-code"
               type="text"
-              placeholder="user id..."
+              placeholder="email..."
               className="input text-white input-bordered input-primary w-full max-w-xs"
             />
           </div>
           <div className="w-full">
             <input
+              onChange={handleInputChange}
+              name='password'
               autoComplete="one-time-code"
               type="password"
-              placeholder="Password..."
+              placeholder="Password"
               className="input text-white input-bordered input-primary w-full max-w-xs"
             />
           </div>
 
           <div className="card-actions w-full mt-4">
-            <button className="btn btn-warning w-full font-bold text-xl">Submit</button>
+            <button
+              onClick={onSubmit}
+              className="btn btn-warning w-full font-bold text-xl"
+            >
+              Submit
+            </button>
           </div>
         </div>
       </div>
